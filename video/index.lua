c7ade51789960fa3f0e988b46b57a36f2b04826d
Noah Pickett <nppickett@gmail.com>
2026-04-15 04:02:26 -0700

    Add Benjamin vs Archanine

--- video/index.lua
+++ video/index.lua
@@ -1,3 +1,4 @@
 return {
-  { id = "bg_fight", title = "Benjamin vs Ichimaruu" }
+  { id = "bg_fight", title = "Benjamin vs Ichimaruu" },
+  { id = "benjamin_vs_archanine", title = "Benjamin vs Archanine" }
 }
