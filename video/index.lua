7a52d33e7d35e889b5575ce1efb5a4172f39ba72
Noah Pickett <nppickett@gmail.com>
2026-04-15 02:53:14 -0700

    Update video catalogue

--- video/index.lua
+++ video/index.lua
@@ -1,1 +1,1 @@
-return {echo   { id = "bg_fight", title = "BG Fight" }echo }
+return {echo   { id = "bg_fight", title = "BG Fight" },echo   { id = "cave", title = "Cave Fight" },echo   { id = "intro", title = "Intro Scene" }echo }
