5a2a2f707c9fe057555020d899c030b1dbf8f25e
Noah Pickett <nppickett@gmail.com>
2026-04-15 04:45:41 -0700

    Add OsamaSon Ref

--- video/index.lua
+++ video/index.lua
@@ -1,5 +1,6 @@
 return {
   { id = "bg_fight", title = "Benjamin vs Ichimaruu" },
   { id = "benjamin_vs_archanine", title = "Benjamin vs Archanine" },
-  { id = "nemesis_mission_revenge", title = "NEMESIS Mission: REVENGE" }
+  { id = "nemesis_mission_revenge", title = "NEMESIS Mission: REVENGE" },
+  { id = "osamason_ref", title = "OsamaSon - Ref MV" }
 }
