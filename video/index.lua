08dfda33aacea2d25bf69a1d6f685909438e0afe
Noah Pickett <nppickett@gmail.com>
2026-04-15 04:57:38 -0700

    Add Backrooms video

--- video/index.lua
+++ video/index.lua
@@ -2,5 +2,6 @@ return {
   { id = "bg_fight", title = "Benjamin vs Ichimaruu" },
   { id = "benjamin_vs_archanine", title = "Benjamin vs Archanine" },
   { id = "nemesis_mission_revenge", title = "NEMESIS Mission: REVENGE" },
-  { id = "osamason_ref", title = "OsamaSon - Ref MV" }
+  { id = "osamason_ref", title = "Osamason - Ref MV" },
+  { id = "aeFUBkFI7wI_video", title = "Benjamin vs PilotBlimp" }
 }
