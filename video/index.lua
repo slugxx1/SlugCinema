6893786a9aceffaa8ce95ae18fa2e6014a460bc5
slugxx1 <nppickett@gmail.com>
2026-04-16 00:09:10 -0700

    Update index.lua

--- video/index.lua
+++ video/index.lua
@@ -3,8 +3,6 @@ return {
   { id = "bg_fight", title = "Benjamin vs Ichimaruu" },
   { id = "aeFUBkFI7wI_video", title = "Benjamin vs PilotBlimp" },
   { id = "nemesis_mission_revenge", title = "NEMESIS Mission: REVENGE" },
-  { id = "nettspend_ft_osamason_wake_up", title = "Nettspend ft. OsamaSon - Wake Up" },
   { id = "nettspend_ft_osamason_wake_up_mv", title = "Nettspend ft. OsamaSon - Wake Up MV" },
   { id = "osamason_ref", title = "Osamason - Ref MV" },
-  { id = "test_video", title = "Test Video" },
 }
