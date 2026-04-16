76b26d429cf02a13d97dc670e8cbc662072db941
Noah Pickett <nppickett@gmail.com>
2026-04-16 13:04:47 -0700

    Add video: Benjamin VS MicWizard (benjamin_vs_micwizard)

--- video/index.lua
+++ video/index.lua
@@ -2,6 +2,7 @@ return {
   { id = "benjamin_vs_archanine", title = "Benjamin vs Archanine" },
   { id = "bg_fight", title = "Benjamin vs Ichimaruu" },
   { id = "benjamin_vs_luther", title = "Benjamin VS Luther" },
+  { id = "benjamin_vs_micwizard", title = "Benjamin VS MicWizard" },
   { id = "aeFUBkFI7wI_video", title = "Benjamin vs PilotBlimp" },
   { id = "nemesis_mission_revenge", title = "NEMESIS Mission: REVENGE" },
   { id = "nettspend_ft_osamason_wake_up_mv", title = "Nettspend ft. OsamaSon - Wake Up MV" },
