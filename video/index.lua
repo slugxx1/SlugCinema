65e91575ba990567ac0902a33284fb69c9d7878c
Noah Pickett <nppickett@gmail.com>
2026-04-17 13:17:37 -0700

    Add video: Benjamin vs Ichimaru (benjaminichimaru)

--- video/index.lua
+++ video/index.lua
@@ -2,6 +2,7 @@ return {
   { id = "backrooms_2", title = "Backrooms Found Footage #2" },
   { id = "backrooms_3", title = "Backrooms Found Footage #3" },
   { id = "benjaminarchanine", title = "Benjamin vs Archanine" },
+  { id = "benjaminichimaru", title = "Benjamin vs Ichimaru" },
   { id = "benjaminluther", title = "Benjamin vs Luther" },
   { id = "benjaminmicwizard", title = "Benjamin vs Mic Wizard" },
   { id = "benjaminpilot", title = "Benjamin vs PilotBlimp" },
