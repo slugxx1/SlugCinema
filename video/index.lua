bf1f9e2ec4962ff2110e07977e1bdf4b877a3430
Noah Pickett <nppickett@gmail.com>
2026-04-17 12:55:16 -0700

    Add video: Benjamin vs Archanine (benjaminarchanine)

--- video/index.lua
+++ video/index.lua
@@ -1,6 +1,7 @@
 return {
   { id = "backrooms_2", title = "Backrooms Found Footage #2" },
   { id = "backrooms_3", title = "Backrooms Found Footage #3" },
+  { id = "benjaminarchanine", title = "Benjamin vs Archanine" },
   { id = "conquestsuperman", title = "Conquest vs Superman" },
   { id = "nemesis_mission_revenge", title = "NEMESIS Mission: REVENGE" },
   { id = "nettspend_ft_osamason_wake_up_mv", title = "Nettspend ft. OsamaSon - Wake Up MV" },
