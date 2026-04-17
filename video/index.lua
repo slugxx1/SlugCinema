ca50d6cfefebabaae9c925cf9883b50e71da7e62
Noah Pickett <nppickett@gmail.com>
2026-04-17 11:57:29 -0700

    Replace videos and update index

--- video/index.lua
+++ video/index.lua
@@ -5,6 +5,11 @@ return {
   { id = "bg_fight", title = "Benjamin vs Ichimaruu" },
   { id = "benjamin_vs_luther", title = "Benjamin VS Luther" },
   { id = "benjamin_vs_micwizard", title = "Benjamin VS MicWizard" },
+<<<<<<< HEAD
+=======
+  { id = "aeFUBkFI7wI_video", title = "Benjamin vs PilotBlimp" },
+  { id = "benjaminpilot", title = "Benjamin vs PilotBlimp" },
+>>>>>>> 73f9d22 (Add video: Benjamin vs PilotBlimp (benjaminpilot))
   { id = "conquestsuperman", title = "Conquest vs Superman" },
   { id = "nemesis_mission_revenge", title = "NEMESIS Mission: REVENGE" },
   { id = "nettspend_ft_osamason_wake_up_mv", title = "Nettspend ft. OsamaSon - Wake Up MV" },
